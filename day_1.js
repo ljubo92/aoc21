de3829da9063607ded745a755348cee27a35b703
Ljubo <36992530+ljubo92@users.noreply.github.com>
2021-12-01 18:06:11 +0100

    Update day_1.js

--- day_1.js
+++ day_1.js
@@ -3,7 +3,7 @@ const data = require("fs").readFileSync("day1.txt", "utf8");
 const a =
     data.split(/\n/g)
         .map(x => parseInt(x))
-        .reduce((prev, _, i, arr) => arr[i] < arr[i+1] ? prev + 1 : prev, 0)
+        .reduce((acc, _, i, arr) => arr[i] < arr[i+1] ? acc + 1 : acc, 0)
 
 console.log(a);
 
@@ -11,6 +11,6 @@ console.log(a);
 const b =
     data.split(/\n/g)
         .map(x => parseInt(x))
-        .reduce((prev, _, i, arr) => arr[i] + arr[i+1] + arr[i+2] < arr[i+1] + arr[i+2] + arr[i+3] ? prev + 1 : prev, 0)
+        .reduce((acc, _, i, arr) => arr[i] + arr[i+1] + arr[i+2] < arr[i+1] + arr[i+2] + arr[i+3] ? acc + 1 : acc, 0)
 
 console.log(b);
